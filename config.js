bfde98f58deed8d2993ceedf3a45e008a7efc940
Bethany Stanley <bethany.stanley@sash.pro>
2021-08-11 16:03:08 -0700

    Facebook OAuth

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
   "secretKey": "12345-67890-09876-54321",
   "mongoUrl": "mongodb://localhost:27017/nucampsite",
+  "facebook": {
+    clientId: "367849644779154",
+    clientSecret: "205f03ec243d4c0ff25612cd76dcced8",
+  },
 };
